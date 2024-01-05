314b22f6ca054b819ae1b12621e1a04293318541
SHADOW <kenkaneki0957@gmail.com>
2024-01-05 11:35:06 +0530

    Starting Array

---
+++ Array.py
@@ -0,0 +1,5 @@
+# # What is an Array?
+# # 1. Array can store data of specified type. 
+# # 2. Element in an array is located in a contiguous. 
+# # 3. Each element of an array has a unique index.
+
