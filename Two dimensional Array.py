d7e078ef0b8f71ec2d5b75c57f907e0b8fc73c08
SHADOW <kenkaneki0957@gmail.com>
2024-01-09 23:39:11 +0530

    Inserting and Appending in 2D Array

--- Two dimensional Array.py
+++ Two dimensional Array.py
@@ -2,3 +2,9 @@
 import numpy as np
 twoDarray = np.array([[11,23,21,8], [23, 19, 5, 9], [25, 31, 1, 17], [15, 0, 4, 41]])
 print(twoDarray)
+
+# twoDarray1 = np.insert(twoDarray, 1, [[1,2,3,4]], axis=0)
+# print(twoDarray1)
+
+twoDarray1 = np.append(twoDarray, [[1,2,3,4]], axis=0)
+print(twoDarray1)
