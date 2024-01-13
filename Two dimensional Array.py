b0ab4de56cba2dca462a933b7e4d74557af9cfde
SHADOW <kenkaneki0957@gmail.com>
2024-01-14 04:40:36 +0530

    Searching in 2D Array

--- Two dimensional Array.py
+++ Two dimensional Array.py
@@ -35,3 +35,18 @@ print(twoDArray)
 #                     # in line 30 its O(n)
 #                     # final  Time Complixity will be O(n∧2)
 # # Space Complixity → O(1)
+
+
+# # Searching Two Dimensional Array
+# def seachTDArray(array, value):
+#     for i in range (len(array)):
+#         for j in range (len(array[0])):
+#             if array[i][j] == value:
+#                 return "The Value is located at index " + str(i) + " " + str(j)
+#     return "Value not found"
+
+# print(seachTDArray(twoDArray, 31))
+
+# # Time Complixity → in line 42 its O(mn) because for every row it should check every column also 
+#                     # in line 43 its O(n)
+# # Space Complixity → O(1)
