c3a853b7ac98c090bba5b7f0b9f931a3977ca110
SHADOW <kenkaneki0957@gmail.com>
2024-01-14 21:39:52 +0530

    Starting Lists

---
+++ Lists.py
@@ -0,0 +1,11 @@
+intergerList = [1,2,3,4]
+print(intergerList)
+
+stringList = ["shadow", "dark", "bright"]
+print(stringList)
+
+mixList = [1, "a", 1.2]
+print(mixList)
+
+nestedList = ["abc", 1, 2, [1.3, 4], ["str", "int"]]
+print(nestedList)
