58d0f11a0b55e002ef03be2cce0c8ccd993af221
SHADOW <kenkaneki0957@gmail.com>
2024-01-15 17:33:20 +0530

    Traversing/Accessing a List

--- Lists.py
+++ Lists.py
@@ -1,11 +1,31 @@
-intergerList = [1,2,3,4]
-print(intergerList)
+# # Creating different Lists
 
-stringList = ["shadow", "dark", "bright"]
-print(stringList)
+# intergerList = [1,2,3,4]
+# print(intergerList)
 
-mixList = [1, "a", 1.2]
-print(mixList)
+# stringList = ["shadow", "dark", "bright"]
+# print(stringList)
 
-nestedList = ["abc", 1, 2, [1.3, 4], ["str", "int"]]
-print(nestedList)
+# mixList = [1, "a", 1.2]
+# print(mixList)
+
+# nestedList = ["abc", 1, 2, [1.3, 4], ["str", "int"]]
+# print(nestedList)
+
+
+# # Accessing/Traversing a List
+
+# shoppingList = ["milk", "ghee", "cheese", "amulButter", "vegtables", "fruits"]
+
+# print(shoppingList[0])
+# print("Butter" in shoppingList) # If this return 'True' means the element is present in list
+# print(shoppingList[-4])
+# for i in shoppingList:
+#     print(i)
+# for i in range (len(shoppingList)):
+#     shoppingList[i] = shoppingList[i] + "+"
+#     print(shoppingList[i])
+
+# emptyList = []
+# for i in emptyList:
+#     print("This will never print. Because list is empty.")
