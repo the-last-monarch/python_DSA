3c2169beab06a0a9d36a3d29da6a622a180e30e4
SHADOW <kenkaneki0957@gmail.com>
2024-02-11 11:08:40 +0530

    Dictionary methods

--- Dictionaries.py
+++ Dictionaries.py
@@ -59,19 +59,59 @@
 
 # # Deleting an element in Dictionary
 
-myDict = {'name': 'Shadow', 'age': 19, 'address': 'Bharat', 'study': 'unpad'}
-del myDict['study']   # Time Complixity = O(1)  Space Complixity = O(1)
-print(myDict)
+# myDict = {'name': 'Shadow', 'age': 19, 'address': 'Bharat', 'study': 'unpad'}
+# del myDict['study']   # Time Complixity = O(1)  Space Complixity = O(1)
+# print(myDict)
 
-remove_element = myDict.pop('study', None)    # Now this will not return any error even if the value is not in the Dictionary and even return value of removed/deleted key.
-print(remove_element)   # Time Complixity = O(1)  Space Complixity = O(1)
-print(myDict)
+# remove_element = myDict.pop('study', None)    # Now this will not return any error even if the value is not in the Dictionary and even return value of removed/deleted key.
+# print(remove_element)   # Time Complixity = O(1)  Space Complixity = O(1)
+# print(myDict)
 
-remove_element_again = myDict.popitem()     # This will remove the last element from the Dictionary
-print(remove_element_again)   # Time Complixity = O(1)  Space Complixity = O(1)
-print(myDict)
+# remove_element_again = myDict.popitem()     # This will remove the last element from the Dictionary
+# print(remove_element_again)   # Time Complixity = O(1)  Space Complixity = O(1)
+# print(myDict)
 
-myDict.clear()
-print(myDict)
+# myDict.clear()    # Time Complixity = O(n)  Space Complixity = O(1)
+# print(myDict)
 
 
+# # Dictionary methods
+
+# myDict = {'name': 'Shadow', 'age': 19, 'address': 'Bharat', 'study': 'unpad'}
+
+# myDict_copy = myDict.copy()
+# print(myDict_copy)
+# print(myDict)
+
+# # newDict = Dictionary.fromkeys(sequence[], values)
+# newDict = {}.fromkeys([1,2,3], 0) # if the vlaue is not given it will automatic set value to none
+# print(newDict)
+
+# # dictionary.get(key, value) If the key is wrong it will return the entered value.
+# print(myDict.get('address', 'india')) # value is optional here but if you enter the wrong value it will return the right value.
+
+# # dictionary.items()
+# print(myDict.items())
+
+# # dictionary.keys()
+# print(myDict.keys())
+
+# # dictionary.popitem()
+# print(myDict.popitem())
+# print(myDict)
+
+# # dictionary.setdefult(key, default_value)   # if the dict contain the key but we enter wrong value it will print the value exist in dict.
+# print(myDict.setdefault('name', 'shado'))     # And if key doesn't exist then it will create another key with the given value and is value isn't given it will enter None
+
+# # dictionary.pop(key, value)
+# print(myDict.pop('name1', 'shakti'))
+
+# # dictionary.values()
+# print(myDict.values())
+
+# dictonary.update(other_dictionary)
+
+# newDict = {'x': 'a', 'y': 'b', 'z': 'c'}
+
+# myDict.update(newDict)
+# print(myDict)
