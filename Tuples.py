1f7f0113679b37e8d69d44aa75e2ded6c726b7f4
SHADOW <kenkaneki0957@gmail.com>
2024-02-21 11:19:37 +0530

    Accessing the tuples

--- Tuples.py
+++ Tuples.py
@@ -1,15 +1,28 @@
 # # What is Tuples?
 # # A tuple is immutable sequence of python objects. Tuples are also comparable and hashable.
 
+# newTuple = ('a', 'b', 'c', 'd', 'e')
+# againTuple = ('a', ) # Tuple with single element
+# anotherTuple = tuple() # Empty tuple
+# onceAgainTuple = tuple('batman') # Now tuple function divide every alphabet in an element here
+
+# print(newTuple)
+# print(againTuple)
+# print(anotherTuple)
+# print(onceAgainTuple)
+
+# # # Time Complexity = O(1)
+# # # Space Complexity = O(n)
+
+# # How to access Tuples
+
 newTuple = ('a', 'b', 'c', 'd', 'e')
-againTuple = ('a', ) # Tuple with single element
-anotherTuple = tuple() # Empty tuple
-onceAgainTuple = tuple('batman') # Now tuple function divide every alphabet in an element here
 
-print(newTuple)
-print(againTuple)
-print(anotherTuple)
-print(onceAgainTuple)
+print(newTuple[1])
+print(newTuple[-3])
 
-# # Time Complexity = O(1)
-# # Space Complexity = O(n)
+# Syntex = tuple[leftIndex:rightIndex] Right Index wouldn't include here
+print(newTuple[1:3])
+print(newTuple[:3])
+print(newTuple[2:])
+print(newTuple[:])
