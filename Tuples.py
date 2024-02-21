f935465344075dfd5905eab218cc16fcffaf3463
SHADOW <kenkaneki0957@gmail.com>
2024-02-21 11:07:30 +0530

    Starting Tuples

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+# # What is Tuples?
+# # A tuple is immutable sequence of python objects. Tuples are also comparable and hashable.
+
+newTuple = ('a', 'b', 'c', 'd', 'e')
+againTuple = ('a', ) # Tuple with single element
+anotherTuple = tuple() # Empty tuple
+onceAgainTuple = tuple('batman') # Now tuple function divide every alphabet in an element here
+
+print(newTuple)
+print(againTuple)
+print(anotherTuple)
+print(onceAgainTuple)
+
+# # Time Complexity = O(1)
+# # Space Complexity = O(n)
