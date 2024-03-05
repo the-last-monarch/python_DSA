75709ed17d24aa5f34f0dca072a0ee1f847cf19f
SHADOW <kenkaneki0957@gmail.com>
2024-03-05 18:05:10 +0530

    Linked List

--- Linked List.py
+++ Linked List.py
@@ -14,5 +14,21 @@ class Node:   # Time Complixity = O(1), Space Complixity = O(1)
         self.value = value
         self.next = None
 
-new_node = Node(10)
-print(new_node)
+# new_node = Node(10)
+# print(new_node)
+
+# class LinkedList:
+#     def __init__(self, value):
+#         new_node = Node(10)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length = 1
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 1
+
+new_linked_list = LinkedList()
+# print(new_linked_list.head.value)
+print(new_linked_list.length)
