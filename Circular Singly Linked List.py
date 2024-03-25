c6df07c7fed2edb5267347eef8232863a8cf6534
SHADOW <kenkaneki0957@gmail.com>
2024-03-25 17:18:46 +0530

    using append method in CSLinkedList

--- Circular Singly Linked List.py
+++ Circular Singly Linked List.py
@@ -18,6 +18,23 @@ class CSLinkedList:
             self.head = None #------------------------- Time Complixity = O(1) Space Complixity = O(1)
             self.tail = None #------------------------- Time Complixity = O(1) Space Complixity = O(1)
             self.length = 0 #-------------------------- Time Complixity = O(1) Space Complixity = O(1)
-
+    
+    
+    def append(self, value):
+        new_node = Node(value) #----------------------- Time Complixity = O(1) Space Complixity = O(1) because we are adding only 1 element in the end 
+        if self.length == 0: #------------------------- Time Complixity = O(1) Space Complixity = O(1)
+            self.head = new_node #--------------------- Time Complixity = O(1) Space Complixity = O(1)
+            self.tail = new_node
+            new_node.next = new_node #----------------- Time Complixity = O(1) Space Complixity = O(1)
+        else:
+            self.tail.next = new_node #---------------- Time Complixity = O(1) Space Complixity = O(1)
+            new_node.next = self.head
+            self.tail = new_node
+        self.length += 1 #----------------------------- Time Complixity = O(1) Space Complixity = O(1)
+        
 cslinkedlist = CSLinkedList()
 print(cslinkedlist.head)
+print(cslinkedlist.append(10))
+print(cslinkedlist.append(20))
+print(cslinkedlist.head.value)
+print(cslinkedlist.head.next.value)
