9d0d4b159a242a8412265726e561989f7d1ad855
SHADOW <kenkaneki0957@gmail.com>
2024-03-28 13:23:58 +0530

    using Prepend Method in DLL

--- Doubly Linked List.py
+++ Doubly Linked List.py
@@ -19,20 +19,31 @@ class DoublyLinkedList:
         while temp_node is not None:
             result += str(temp_node.value)
             if temp_node.next is not None:
-                result += "<->"
+                result += " <-> "
             temp_node = temp_node.next
         return result
+    
+    def prepend(self, value):
+        new_node = Node(value) #-------------------------------- Time Complixity = O(1) Space Complixity = O(1)
+        if self.head is None:
+            self.head = new_node #------------------------------ Time Complixity = O(1) Space Complixity = O(1)
+            self.tail = new_node
+        else: #------------------------------------------------- Time Complixity = O(1) Space Complixity = O(1)
+            new_node.next = self.head
+            self.head.prev = new_node #------------------------- Time Complixity = O(1) Space Complixity = O(1)
+            self.head = new_node
+        self.length += 1
 
     def append(self, value):
-        new_node = Node(value) #-------------------------------- Time Complixity = O(n) Space Complixity = O(1)
+        new_node = Node(value) #-------------------------------- Time Complixity = O(1) Space Complixity = O(1)
         if not self.head:
-            self.head = new_node #------------------------------ Time Complixity = O(n) Space Complixity = O(1)
+            self.head = new_node #------------------------------ Time Complixity = O(1) Space Complixity = O(1)
             self.tail = new_node
         else:
-            self.tail.next = new_node #------------------------- Time Complixity = O(n) Space Complixity = O(1)
+            self.tail.next = new_node #------------------------- Time Complixity = O(1) Space Complixity = O(1)
             new_node.prev = self.tail
             self.tail = new_node
-        self.length += 1 #-------------------------------------- Time Complixity = O(n) Space Complixity = O(1)
+        self.length += 1 #-------------------------------------- Time Complixity = O(1) Space Complixity = O(1)
 
 # new_node = Node(10)
 # print(new_node)
@@ -41,4 +52,6 @@ newDDL = DoublyLinkedList()
 newDDL.append(10)
 newDDL.append(20)
 newDDL.append(30)
+print(newDDL)
+newDDL.prepend(40)
 print(newDDL)
