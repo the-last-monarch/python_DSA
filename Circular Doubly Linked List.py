3fd37a2b891ab372aa7e2028fc3c5b5e0dda13f3
SHADOW <kenkaneki0957@gmail.com>
2024-03-31 10:51:24 +0530

    using append method in CDLL

--- Circular Doubly Linked List.py
+++ Circular Doubly Linked List.py
@@ -1,6 +1,6 @@
 class Node:
     def __init__(self, value):
-        self.value = value #------------------------------------ Time Complixity = O(n) because we are creating new Node Space Complixity = O(1)
+        self.value = value #----------------------------------------------- Time Complixity = O(n) because we are creating new Node Space Complixity = O(1)
         self.next = None
         self.prev = None
     
@@ -8,18 +8,36 @@ class Node:
         return str(self.value)
 
 class CircularDoublyLinkedList():
-    # def __init__(self):
-    #     self.head = None
-    #     self.tail = None
-    #     self.length = 0
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-    def __init__(self, value):
-        new_node = Node(value)
-        new_node.next = new_node
-        new_node.prev = new_node
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+    # def __init__(self, value):
+    #     new_node = Node(value)
+    #     new_node.next = new_node
+    #     new_node.prev = new_node
+    #     self.head = new_node
+    #     self.tail = new_node
+    #     self.length = 1
+    
+    def append(self, value):
+        new_node = Node(value) #------------------------------------------- Time Complixity = O(1) Space Complixity = O(1)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node #----------------------------------------- Time Complixity = O(1) Space Complixity = O(1)
+            new_node.next = new_node
+            new_node.prev = new_node
+        else:
+            self.tail.next = new_node
+            self.head.prev = new_node #------------------------------------ Time Complixity = O(1) Space Complixity = O(1)
+            new_node.prev = self.tail
+            new_node.next = self.head
+            self.tail = new_node
+        self.length += 1 #------------------------------------------------- Time Complixity = O(1) Space Complixity = O(1)
         
-new_cdll = CircularDoublyLinkedList(10)
-print(new_cdll.length)
+new_cdll = CircularDoublyLinkedList()
+
+new_cdll.append(10)
+new_cdll.append(20)
+print(new_cdll.tail)
