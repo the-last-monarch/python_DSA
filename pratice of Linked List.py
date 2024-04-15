dd670ea928b5b2f035fc22c6142ff90cda30bb1e
SHADOW <kenkaneki0957@gmail.com>
2024-04-15 17:39:28 +0530

    Partition

--- pratice of Linked List.py
+++ pratice of Linked List.py
@@ -28,28 +28,51 @@ from LinkedList import LinkedList
 # # Question 2 :-
 # # Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x.
 
-# Solution 2 :- 
+# # Solution 2 :- 
 
-def partition(ll, x):
-    current_node = ll.head #------------------------------ Time Complixity = O(1)  Space Complixity = O(1)
-    ll.tail = ll.head
+# def partition(ll, x):
+#     current_node = ll.head #------------------------------ Time Complixity = O(1)  Space Complixity = O(1)
+#     ll.tail = ll.head
     
-    while current_node: #--------------------------------- Time Complixity = O(n) because of "while loop"  Space Complixity = O(1)
-        next_node = current_node.next
-        current_node.next = None
-        if current_node.value < x: #---------------------- Time Complixity = O(1)  Space Complixity = O(1)
-            current_node.next = ll.head
-            ll.head = current_node
-        else: #------------------------------------------- Time Complixity = O(1)  Space Complixity = O(1)
-            ll.tail.next = current_node
-            ll.tail = current_node
-        current_node = next_node
+#     while current_node: #--------------------------------- Time Complixity = O(n) because of "while loop"  Space Complixity = O(1)
+#         next_node = current_node.next
+#         current_node.next = None
+#         if current_node.value < x: #---------------------- Time Complixity = O(1)  Space Complixity = O(1)
+#             current_node.next = ll.head
+#             ll.head = current_node
+#         else: #------------------------------------------- Time Complixity = O(1)  Space Complixity = O(1)
+#             ll.tail.next = current_node
+#             ll.tail = current_node
+#         current_node = next_node
     
-    if ll.tail.next is not None: #------------------------ Time Complixity = O(1)  Space Complixity = O(1)
-        ll.tail.next = None
+#     if ll.tail.next is not None: #------------------------ Time Complixity = O(1)  Space Complixity = O(1)
+#         ll.tail.next = None
 
-custom_ll = LinkedList()
-custom_ll.generate(10, 0, 99)
-print(custom_ll)
-partition(custom_ll, 30)
-print(custom_ll)
+# custom_ll = LinkedList()
+# custom_ll.generate(10, 0, 99)
+# print(custom_ll)
+# partition(custom_ll, 30)
+# print(custom_ll)
+
+
+# # Question 3 :-
+# # You have 2 number represented by linked list, where each node contains single digit. The digits are stored in reverse order, 
+# # such that the 1's digit is at the head of list. Write a function that adds the two numbers and returns the sum as a linked list.
+
+# # Answer 3 :- 
+
+def sumLists(llA, llB):
+    temp_node1 = llA.head
+    temp_node2 = llB.head
+    carry = 0
+    ll = LinkedList()
+    
+    while llA or llB:
+        result = carry
+        
+        if temp_node1:
+            result += temp_node1.value
+            temp_node1 = temp_node1.next
+        if temp_node2:
+            result += temp_node2.value
+            temp_node2 = temp_node2.next
