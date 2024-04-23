0160f9003669abd4a353833d13a2e8dc9c4097a4
SHADOW <kenkaneki0957@gmail.com>
2024-04-23 20:15:12 +0530

    Operation on Stack using Linked List

--- StackList.py
+++ StackList.py
@@ -128,3 +128,57 @@ class LinkedList():
         while currentNode:
             yield currentNode
             currentNode = currentNode.next
+
+
+# Operation on Stack using Linked List
+
+class Stack:
+    def __init__(self):
+        self.LinkedList = LinkedList()
+
+    def __str__(self) -> str: # ---------------------> Time Complixety = O(1), Space Complixety = O(1)
+        values = [str(x.value) for x in self.LinkedList]
+        return '\n'.join(values)
+    
+    def isEmpty(self): # ----------------------------> Time Complixety = O(1), Space Complixety = O(1)
+        if self.LinkedList.head == None:
+            return True
+        else:
+            return False
+    
+    def push(self, value): # ------------------------> Time Complixety = O(1), Space Complixety = O(1)
+        node = Node(value)
+        node.next = self.LinkedList.head
+        self.LinkedList.head = node
+    
+    def pop(self): # --------------------------------> Time Complixety = O(1), Space Complixety = O(1)
+        if self.isEmpty():
+            return "There is not any element in stack"
+        else:
+            nodeValue = self.LinkedList.head.value
+            self.LinkedList.head = self.LinkedList.head.next
+            return nodeValue
+            
+    def peek(self): # -------------------------------> Time Complixety = O(1), Space Complixety = O(1)
+        if self.isEmpty():
+            return "There is not any element in stack"
+        else:
+            nodeValue = self.LinkedList.head.value
+            return nodeValue 
+    
+    def delete(self): # -----------------------------> Time Complixety = O(1), Space Complixety = O(1)
+        self.LinkedList.head = None           
+            
+
+customStack = Stack()
+# print(customStack.isEmpty())
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+# print(customStack)
+# print("----------")
+# customStack.pop()
+# print(customStack)
+# print(customStack.peek())
+print(customStack.delete())
+print(customStack)
